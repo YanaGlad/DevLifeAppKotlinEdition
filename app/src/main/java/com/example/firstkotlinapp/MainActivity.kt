8c0c7d72048a2162d5150f1400eca096301faf3f
YanaGlad <olesya_leader@mail.ru>
2021-07-11 15:35:33 +0300

    starting the work

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.hello)
+        textView.setText("Goodbye")
     }
 }
